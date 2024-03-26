bba7dc23e0ce25a897d83c20d32c3e04c4de2cf4
Kim SeungHyun <korafa@naver.com>
2024-03-26 14:25:43 +0900

    초기 커밋.

---
+++ ClimbStairs/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ClimbStairs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"50개의 계단을 올라가는 방법의 수는 : {ClimbStairs(50)}");
+        }
+
+        public static long ClimbStairs(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+
+            long[] differentMethod = new long[n + 1];
+            differentMethod[1] = 1;
+            differentMethod[2] = 2;
+
+            for (int i = 3; i <= n; i++)
+            {
+                differentMethod[i] = differentMethod[i - 1] + differentMethod[i - 2];
+            }
+            return differentMethod[n];
+        }
+    }
+}
